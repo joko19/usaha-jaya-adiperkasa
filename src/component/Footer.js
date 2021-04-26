eb073bf47d3327ab23728c6ad319c8f92ab7e483
joko19 <jokoslamet459@gmail.com>
2021-04-26 23:02:33 +0700

    material ui

---
+++ src/component/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div style={{ backgroundColor: "#2E3E5C", color: "white" }}>
+      Ini adalah Footer
+    </div>
+  );
+}
+
+export default Footer;
--- src/component/Home.js
+++ src/component/Home.js
@@ -1,7 +1,15 @@
 import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
 
 function Home() {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <Header />
+      hello world hello world hello world hello world hello world
+      <Footer />
+    </div>
+  );
 }
 
 export default Home;

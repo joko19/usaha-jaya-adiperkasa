74f61d422946b15b40dea15d1fa07abb9ce01d08
joko19 <jokoslamet459@gmail.com>
2021-05-01 00:38:16 +0700

    about us

--- src/component/About.js
+++ src/component/About.js
@@ -1,7 +1,239 @@
 import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import "./../style/About.css";
+import lusia from "./../img/photo/lusia.png";
+import auw from "./../img/photo/auw.png";
+import ike from "./../img/photo/ike.png";
+import ida from "./../img/photo/ida.png";
+import aditya from "./../img/photo//aditya.png";
+import frans from "./../img/photo/frans.png";
+import maulintang from "./../img/photo/maulintang.png";
+import fitri from "./../img/photo/fitri.png";
+import hayri from "./../img/photo/hayri.png";
+import sriwijaya from "./../img/photo/sriwijaya.png";
+import yohanes from "./../img/photo/yohanes.png";
+import andika from "./../img/photo/andhika.png";
+import kardi from "./../img/photo/kardi.png";
+import daniella from "./../img/photo/daniella.png";
+import andri from "./../img/photo/andri.png";
+import karno from "./../img/photo/karno.png";
+import urip from "./../img/photo/urip.png";
+import fredy from "./../img/photo/fredy.png";
+import dedik from "./../img/photo/dedik.png";
+import nico from "./../img/photo/nico.png";
+import suyadi from "./../img/photo/suyadi.png";
+import suparian from "./../img/photo/suparian.png";
+import imron from "./../img/photo/imron.png";
+import edi from "./../img/photo/edi.png";
+import buriman from "./../img/photo/buriman.png";
+import hartono from "./../img/photo/hartono.png";
+import michael from "./../img/photo/michael.png";
+import ali from "./../img/photo/ali.png";
+import santoso from "./../img/photo/santoso.png";
+import ismuntoro from "./../img/photo/ismuntoro.png";
 
 function About() {
-  return <div>Halaman About Us</div>;
+  const team = [
+    {
+      img: lusia,
+      name: "Lusia Marganitha",
+      position: "PIC Admin",
+    },
+    {
+      img: auw,
+      name: "Auw Ing Ing",
+      position: "Admin",
+    },
+    {
+      img: ike,
+      name: "Ike Damayanti",
+      position: "Admin",
+    },
+    {
+      img: ida,
+      name: "Ida Agus Rosalina",
+      position: "Admin",
+    },
+    {
+      img: aditya,
+      name: "Aditya Mahardika",
+      position: "PIC Management & Design",
+    },
+    {
+      img: frans,
+      name: "Frans Santoso",
+      position: "Management",
+    },
+    {
+      img: maulintang,
+      name: "Maulintang Larassati",
+      position: "Design",
+    },
+    {
+      img: fitri,
+      name: "Fitri Diah Wulandari",
+      position: "Design",
+    },
+    {
+      img: hayri,
+      name: "Hayri Sutandar",
+      position: "PIC Marketing",
+    },
+    {
+      img: sriwijaya,
+      name: "Sriwijaya",
+      position: "Admin Sales",
+    },
+    {
+      img: yohanes,
+      name: "Yohanes Harianto",
+      position: "Marketing",
+    },
+    {
+      img: andika,
+      name: "Andhika Pristyawan P.",
+      position: "Marketing",
+    },
+    {
+      img: kardi,
+      name: "Kardi Priyanto Sutardji",
+      position: "Marketing",
+    },
+    {
+      img: daniella,
+      name: "Daniella Sukma H.",
+      position: "Marketing",
+    },
+    {
+      img: andri,
+      name: "Andri",
+      position: "Marketing",
+    },
+    {
+      img: karno,
+      name: "Karno",
+      position: "Marketing End User",
+    },
+    {
+      img: urip,
+      name: "Mochammad Urip S.",
+      position: "Marketing End User",
+    },
+    {
+      img: fredy,
+      name: "Fredy",
+      position: "Support",
+    },
+    {
+      img: dedik,
+      name: "Dedik Kurniawan",
+      position: "Support",
+    },
+    {
+      img: nico,
+      name: "Nico Hermnsyah",
+      position: "",
+    },
+    {
+      img: suyadi,
+      name: "Suyadi",
+      position: "PIC Support",
+    },
+    {
+      img: suparian,
+      name: "PIC Warehouse",
+      position: "",
+    },
+    {
+      img: imron,
+      name: "Muhammad Imron",
+      position: "Warehouse",
+    },
+    {
+      img: edi,
+      name: "Edi Lesmana",
+      position: "Warehouse",
+    },
+    {
+      img: buriman,
+      name: "Buriman",
+      position: "Warehouse",
+    },
+    {
+      img: hartono,
+      name: "Hartono",
+      position: "Warehouse",
+    },
+    {
+      img: michael,
+      name: "Michael",
+      position: "Warehouse",
+    },
+    {
+      img: ali,
+      name: "Moh. Ali",
+      position: "Driver",
+    },
+    {
+      img: santoso,
+      name: "Edi Santoso",
+      position: "PIC Driver",
+    },
+    {
+      img: ismuntoro,
+      name: "Ismuntoro",
+      position: "Driver",
+    },
+  ];
+  return (
+    <div>
+      <Header />
+      <div className="cover">
+        <h1 className="cover-title">Tentang Kami</h1>
+      </div>
+      <p className="desc-first">
+        Sebagai bagian dari Usaha Jaya Group{" "}
+        <strong className="desc-bold">PT Usaha Jaya Adiperkasa (PT UJA)</strong>
+        adalah salah satu perusahaan import dan distribusi yang berpengalaman,
+        ahli dalam hal pengadaan alat, perangkat dan suku cadang untuk pabrik,
+        konstruksi, Galangan kapal dan industri manufaktur. PT UJA beroperasi
+        dan memiliki jaringan yang tersebar di seluruh Indonesia, khususnya
+        wilayah Indonesia Timur.
+      </p>
+
+      <p className="desc">
+        PT UJA menyatukan aspirasi dan kebutuhan masyarakat Indonesia dengan
+        memasok berbagai macam produk berkualitas dan mendistribusikannya secara
+        luas di seluruh wilayah Indonesia serta tetap mengutamakan dalam hal
+        memberikan pelayanan serta menjaga hubungan kerjasama yang baik di
+        antara para rekan kerja, pelanggan & konsumen yang ada.{" "}
+      </p>
+      <p className="desc">
+        Dengan adanya kualitas produk yang dapat diandalkan dan terjaga, PT UJA
+        berharap dapat terus membangun dan menjaga hubungan baik dengan para
+        pemasok dari mancanegara, seperti Jepang, Jerman, Inggris, Tiongkok,
+        Taiwan, Australia, Italia dan tidak menutup kemungkinan adanya pemasok
+        dari negara-negara lain nantinya.
+      </p>
+      <center>
+        {team.map((item) => {
+          return <Photo item={item} />;
+        })}
+      </center>
+      <Footer />
+    </div>
+  );
 }
+
+const Photo = (props) => {
+  return (
+    <div className="photo">
+      <img src={props.item.img} className="photo-img" />
+      <h6 className="photo-name">{props.item.name}</h6>
+      <p className="photo-position">{props.item.position}</p>
+    </div>
+  );
+};
 
 export default About;

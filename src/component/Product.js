b9d62360197b6ae427dd134020c0169e971ef8e9
joko19 <jokoslamet459@gmail.com>
2021-05-03 11:28:46 +0700

    about us

--- src/component/Product.js
+++ src/component/Product.js
@@ -1,7 +1,18 @@
 import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
 
 function Product() {
-  return <div>Halaman Produk</div>;
+  return (
+    <div>
+      <Header />
+
+      <div className="cover">
+        <h1 className="cover-title">Produk Kami</h1>
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default Product;
